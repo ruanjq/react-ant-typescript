c391a8a882927bb02fa5faad0c10a72f52a9cdb7
ruanjiqian <1016744743@qq.com>
2020-08-09 15:56:59 +0800

    提交demo

--- src/views/Test.tsx
+++ src/views/Test.tsx
@@ -3,10 +3,10 @@ import React, { useState,useRef, useEffect, FC, useCallback } from "react";
 
 const Child = React.memo((props:any) => {
   console.log("子组件props", props)
-  const {name} = props;
+  const {name,list} = props;
   const showName = useCallback(() => {
-    console.log("子组件的name", name)
-  },[name])
+    console.log("子组件的name", list.length)
+  },[list])
   return <div>
     <button onClick={props.onChange}>回调</button>
     <button onClick={showName}>打印父组件的name</button>
@@ -18,11 +18,13 @@ const Child = React.memo((props:any) => {
 export default function App() {
   const [count,setCount] = useState(0)
   const [name,setName] = useState("哈哈")
+  const [list ,setList] = useState([])
   const handleCount = (e:any) => {
     setCount(c => c + 1)
   }
 
   const handleName = (e:any) => {
+    setList([12])
     setName("呵呵")
   }
   const handleChange = useCallback(() => {
@@ -32,7 +34,7 @@ export default function App() {
     <h2>我是父组件 {count}</h2>
     <button onClick={e => handleCount(e)}>更改count</button>
     <button onClick={e => handleName(e)}>更改name</button>
-    <Child name={name} onChange={handleChange}/>
+    <Child name={name} list={list} onChange={handleChange}/>
   </div>
 }
 
