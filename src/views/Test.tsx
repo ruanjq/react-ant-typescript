95123dc46fee60239c21bc8ef4052f231b85d04e
ruanjiqian <1016744743@qq.com>
2020-08-10 22:55:18 +0800

    test

--- src/views/Test.tsx
+++ src/views/Test.tsx
@@ -1,40 +1,44 @@
-import React, { useState,useRef, useEffect, FC, useCallback } from "react";
+import React, { useState, useRef, useEffect, FC, useCallback } from "react";
 
 
-const Child = React.memo((props:any) => {
+const Child = React.memo((props: any) => {
   console.log("子组件props", props)
-  const {name,list} = props;
-  const showName = useCallback(() => {
-    console.log("子组件的name", list.length)
-  },[list])
+  const { list, active } = props
+  const handleClick = (item,e) => {
+    props.onChange(item)
+  }
   return <div>
-    <button onClick={props.onChange}>回调</button>
-    <button onClick={showName}>打印父组件的name</button>
-    <h2>父组件传递回来的name ： {name}</h2>
+    {
+      list.map((item, index) => {
+        return <div key={index} style={{backgroundColor: active === item ? 'red': 'transparent'}}><button onClick={e => handleClick(item,e)}>回调{item}</button> <br/></div>
+      })
+    }
   </div>
 })
 
 
 export default function App() {
-  const [count,setCount] = useState(0)
-  const [name,setName] = useState("哈哈")
-  const [list ,setList] = useState([])
-  const handleCount = (e:any) => {
-    setCount(c => c + 1)
+  let [active, setActive] = useState(1)
+  const [list, setList] = useState([1, 2, 3, 4, 5])
+  
+  const showActive = () => {
+    console.log("当前key", active)
   }
 
-  const handleName = (e:any) => {
-    setList([12])
-    setName("呵呵")
+  const handleChange = (key) => {
+    console.log("key", key)
+    setActive(key)
   }
-  const handleChange = useCallback(() => {
-    console.log("父组件的回调函数")
-  },[])
+  const add = () => {
+    active +=1
+    handleChange(active)
+  }
   return <div>
-    <h2>我是父组件 {count}</h2>
-    <button onClick={e => handleCount(e)}>更改count</button>
-    <button onClick={e => handleName(e)}>更改name</button>
-    <Child name={name} list={list} onChange={handleChange}/>
+    <h2>当前选中的key{active}</h2>
+    <button onClick={add}>key 地增</button>
+    <button onClick={showActive}>打印</button>
+    <hr />
+    <Child active={active} list={list} onChange={handleChange} />
   </div>
 }
 
